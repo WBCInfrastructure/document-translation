99007159368b1086f26d8601c168a797cde568cb
WBCInfrastructure <infrastructure.team@wokingham.gov.uk>
2025-02-11 11:43:48 +0000

    Update view.tsx
    Add Download button

--- website/src/page/readable/view.tsx
+++ website/src/page/readable/view.tsx
@@ -90,6 +90,56 @@ export default function ReadableNew() {
 		return hasInput || hasOutput;
 	}
 
+		const handleTableDownload=() => {
+		// generate html content
+		const htmlTextContent = generateHtmlTextContent(textState);
+		const tableBlob = new Blob([htmlTextContent], { type: 'text/html' });
+		//create link
+		const tableLink = document.createElement('a');
+		tableLink.href = URL.createObjectURL(tableBlob);
+		tableLink.download = 'table.html';  
+		// pop link on doc and trigger click
+		document.body.appendChild(tableLink);
+		tableLink.click();
+		document.body.removeChild(tableLink);   
+	};
+	const generateHtmlTextContent = (data) => {
+		//create html table and set inline styles
+		//replace new lines with breaks
+		// take care of any empty items when replacing lines with breaks 
+		const tableContent = data
+			.filter(item => item && item.output !== null)
+			.map((item) => (
+			`<tr>
+				<td style ="font-size:5px; color: white;">${item.itemId}</td>
+				<td style ="font-size:24px;">${item.output.replace(/\n/g, '<br>')}</td>
+			</tr>`
+		)).join('');
+		
+		// wrap in html document 
+		const htmlDocument = `
+			<!DOCTYPE html>
+			<html>
+				<head>
+					<title>Simply Readable Text Output</title>
+				</head>
+				<body>
+					<table>
+						<thead>
+							<tr>
+								<th>Image</th>
+								<th>Text</th>
+							</tr>
+						</thead>
+						<tbody>
+							${tableContent}
+						</tbody>
+					</table>
+				</body>
+			</html>`;
+		return htmlDocument;
+	};
+
 	async function createNewTextItem(order) {
 		const authSession = await fetchAuthSession();
 		try {
@@ -333,6 +383,21 @@ export default function ReadableNew() {
 			</>
 		);
 	}
+	function displayTextTableDownloadButton() {
+		return (
+			<>
+				<Box variant="div" textAlign="left">
+					<Button
+						iconName="external"
+						variant="link"
+						onClick={handleTableDownload}
+					>
+					{t("Download")}
+					</Button>
+				</Box>
+			</>
+		);
+	}
 	function displayAddImageRow(textItem, index) {
 		return (
 			<>
@@ -536,7 +601,14 @@ export default function ReadableNew() {
 								</SpaceBetween>
 							)))}
 					</SpaceBetween>
-					{displayAddTextRow()}
+					<Grid gridDefinition={[{ colspan: 6 }]}>
+						<Box>
+							{displayAddTextRow()}
+						</Box>
+						<Box>
+							{displayTextTableDownloadButton()}
+						</Box>
+					</Grid>
 				</SpaceBetween>
 			</ContentLayout>
 		</>
