ebfd399b8242ca17c8eac45b30043b7ff942cd04
WBCInfrastructure <infrastructure.team@wokingham.gov.uk>
2025-03-03 14:30:47 +0000

    Update historyTable.tsx

--- website/src/page/translation/historyTable.tsx
+++ website/src/page/translation/historyTable.tsx
@@ -20,13 +20,10 @@ import {
 
 import { useTranslationJobs } from "./hooks/useTranslationJobs";
 
-import { amplifyConfigureAppend } from "../../util/amplifyConfigure";
 import { formatJobNameId } from "../../util/formatJobNameId";
 import { formatTimestamp } from "../../util/formatTimestamp";
-import { getPresignedUrl } from "../../util/getPresignedUrl";
 import { describeS3Key } from "./util/describeS3Key";
 
-const cfnOutputs = require("../../cfnOutputs.json");
 
 interface Item {
 	createdAt: number;
@@ -39,16 +36,9 @@ interface Item {
 }
 
 export default function HistoryTable() {
-	const storageConfig = {
-		Storage: {
-			S3: {
-				bucket: cfnOutputs.awsUserFilesS3Bucket,
-				region: cfnOutputs.awsRegion,
-			},
-		},
-	};
-	amplifyConfigureAppend(storageConfig);
-	const { jobs, loading } = useTranslationJobs();
+
+
+	const { jobs, loading, fetchJobs } = useTranslationJobs();
 	const { t } = useTranslation();
 	const navigate = useNavigate();
 	const [hideExpired, setHideExpired] = useState(true);
@@ -136,10 +126,7 @@ export default function HistoryTable() {
 				const k = describeS3Key({
 					key: keys[i],
 				});
-				const presignedUrl = await getPresignedUrl({
-					path: `${k.scope}/${k.identity}/${k.jobId}/${k.stage}/${k.translateId}/${k.filename}`,
-					bucketKey: "awsUserFilesS3Bucket",
-				});
+				const presignedUrl = null;
 				window.open(presignedUrl, "_blank", "noopener,noreferrer");
 			}
 		} catch (err) {
@@ -185,7 +172,6 @@ export default function HistoryTable() {
 				return !hideExpired || item.jobStatus.toUpperCase() !== "EXPIRED";
 			})}
 			loadingText={t("generic_loading")}
-			loading={loading}
 			trackBy="id"
 			empty={
 				<Box margin={{ vertical: "xs" }} textAlign="center" color="inherit">
@@ -202,19 +188,31 @@ export default function HistoryTable() {
 			}
 			header={
 				<Header
-				counter={`(${jobs.length})`}
-				actions={
-					<Toggle
-						onChange={({ detail }) => toggleExpired(detail.checked)}
-						checked={hideExpired ? false : true}
+				  counter={`(${jobs.length})`}
+				  actions={
+					<SpaceBetween 
+						direction="horizontal" 
+						size="s" 
+						alignItems="center"
 					>
+					  <Button
+						onClick={ () => fetchJobs()}
+					  >
+						{t("generic_refresh")}
+					  </Button>						
+					  <Toggle
+						onChange={({ detail }) => toggleExpired()}
+						checked={hideExpired ? false : true}
+					  >
 						{t("generic_status_expired")}
-					</Toggle>
-				}
+					  </Toggle>
+					</SpaceBetween>
+				  }
 				>
-					{t("generic_history")}
+				  {t("generic_history")}
 				</Header>
-			}
+			  }
+			  
 			stickyHeader
 			stripedRows
 			filter={
