2a1dc78ecdb4f6d27012242a708cd947f3ee1b1f
WBCInfrastructure <infrastructure.team@wokingham.gov.uk>
2025-03-03 16:30:08 +0000

    Update useTranslationJobs.tsx

--- website/src/page/translation/hooks/useTranslationJobs.tsx
+++ website/src/page/translation/hooks/useTranslationJobs.tsx
@@ -1,9 +1,14 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
 import { useEffect, useState } from "react";
+
 import { generateClient } from "@aws-amplify/api";
 
+const features = require("../../../features.json");
 let listJobs: string;
+if (features.translation) {
+	listJobs = require("../../../graphql/queries").translationListJobs;
+}
 
 const client = generateClient({ authMode: "userPool" });
 
@@ -11,29 +16,23 @@ export const useTranslationJobs = () => {
 	const [jobs, updateJobs] = useState([]);
 	const [loading, setLoading] = useState<Boolean>(true);
 
-	// Function to fetch jobs
-	const fetchJobs = async () => {
-		setLoading(true); // Set loading to true while fetching data
-		try {
-		const response = await client.graphql({
-			query: listJobs,
-		});
-		let data: any;
-		if ("data" in response) {
-			data = response.data.translationListJobs.items;
+	useEffect(() => {
+		async function fetchJobs() {
+			try {
+				const response = await client.graphql({
+					query: listJobs,
+				});
+				let data: any;
+				if ("data" in response) {
+					data = response.data.translationListJobs.items;
+				}
+				updateJobs(data);
+			} catch (error) {}
+			setLoading(false);
+			return true;
 		}
-		updateJobs(data); // Update the jobs state
-		} catch (error) {
-		console.error("Error fetching jobs:", error); // Log any errors
-		} finally {
-		setLoading(false); // Set loading to false once data is fetched
-		}
-	};
+		fetchJobs();
+	}, []);
 
-	 // Fetch jobs initially when the component mounts
-  	useEffect(() => {
-    	fetchJobs();
-  	}, []);
-
-	return { jobs, loading, fetchJobs };
+	return { jobs, loading };
 };
